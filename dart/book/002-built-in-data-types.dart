6e546e847d2bb3df985bbd85e8eddf553aa1c253
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-24 19:23:11 +0500

    create buid in data typed file

---
+++ dart/book/002-built-in-data-types.dart
@@ -0,0 +1,46 @@
+// void main(){
+//   // Numbers: int
+//   int score = 123;
+//   var count = 123; // it is inferred as integers automatically by compiler
+//   int hexValue = 0xEADEBAEE;
+
+//   // Numbers: double
+//   double percentage = 93.4;
+//   var percent = 93.4;
+//   double exponents = 1.42e5;
+
+
+//   // String
+//   String name = "Abubakar";
+//   var company = "Abubakar";
+
+//   // Booleans
+//   bool isValid = true;
+//   var isAlive = false;
+
+
+
+//   // print
+//   print(score);
+//   print(count);
+//   print(hexValue);
+//   print(percentage);
+//   print(exponents);
+//   print(name);
+//   print(isValid);
+
+//   // Note: All data typed in dart are Objects
+//   // therefor, their initial value by default null
+// }
+
+
+// void main(){
+
+//   var num = "45" * "46";
+//   print(num);
+// }
+
+void main(){
+  double  nine_point_five = 9.5;
+  print(" Hey this is object{$nine_point_five + 100}");
+}
