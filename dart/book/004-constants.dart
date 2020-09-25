4a86823b252a885e9d4bb4b1c1a92999539dd65e
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-25 12:08:04 +0500

    create 004-constants file

---
+++ dart/book/004-constants.dart
@@ -0,0 +1,22 @@
+void main(){
+  // final
+  final cityName = 'Karachi';
+  print(cityName);
+
+  // name = "peter" //throw an error
+
+  final countryName = 'Pakistan';
+  print(countryName);
+
+  // const
+  const PI = 3.14;
+  const double gravity = 9.8;
+  print(PI);
+  print(gravity);
+
+}
+class circle {
+  final color = 'Red';
+  static const PI = 3.14;
+
+}
