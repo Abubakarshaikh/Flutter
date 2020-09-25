48a975c7b61d858676769054c8bdcbe68d2cf48a
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-25 12:20:13 +0500

    create 005-if-else-control-flow file

---
+++ dart/book/005-if-else-control-flow.dart
@@ -0,0 +1,39 @@
+void main(){
+  // If and Else statement
+  // var salary = 15000;
+
+  // if (salary > 20000) {
+  //   print("You got Promotion Congratulations !");
+  // }
+  // else {
+  //   print("You need to Work hard");
+ 
+
+
+  // If Else ladder statement
+  var marks = 40;
+  if (marks >= 90 && marks < 100){
+    print("A+ Grade!");
+  }
+  else if (marks >= 80 && marks < 90){
+    print("A Grade!");
+  }
+  else if (marks >= 70 && marks < 80){
+    print("B Grade!");
+  }
+  else if (marks >= 60 && marks < 70){
+    print("C Grade!");
+  }
+  else if (marks >= 30 && marks < 60){
+    print("D Grade!");
+  }
+  else if (marks >= 0 && marks < 30){
+    print("You have failed");
+  }
+  else {
+    print("Invalid marks, Please Try again");
+  }
+
+
+
+}
