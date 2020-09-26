646fe37a539e32e0cb85fec9c0ab23697f53bf6a
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-26 11:39:51 +0500

    007-switch-and-case.dart file

---
+++ dart/book/007-switch-and-case.dart
@@ -0,0 +1,27 @@
+void main(){
+  // Switch Case Statements: Applicable for only 'int' and 'String'
+
+  String grade = 'B';
+
+  switch (grade) {
+    case 'A':
+    print("Execelent grade of A");
+    break;
+
+    case 'B':
+    print("Very good");
+    break;
+
+    case 'C':
+    print("Good enough, but work hard");
+    break;
+
+    case 'F':
+    print("You have failed");
+    break;
+
+    default:
+    print("Invalid Grade");
+  }
+
+}
