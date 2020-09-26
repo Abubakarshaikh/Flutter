d2df16ebe6eb64685ea9c4c2e4148140bd300903
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-26 11:50:16 +0500

    009-while-loop.dart file

---
+++ dart/book/009-while-loop.dart
@@ -0,0 +1,13 @@
+void main(){
+  // While loop
+  // WAP to find the even numbers between 1 tp 10
+  var i = 1;
+  while( i <= 10 ){
+    if(i % 2 == 0 ){
+      print(i);
+    }
+
+    i++;
+
+  }
+}
